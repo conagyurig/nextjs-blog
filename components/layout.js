1aac94159269fadd491f07db1211cf717bf3db09
conagyurig <youngcraig23@gmail.com>
2022-12-18 17:53:40 +0000

    MD-1 - try feature branch deployment

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Emily';
+const name = 'Emily and Craig';
 export const siteTitle = 'Development';
 
 export default function Layout({ children, home }) {
